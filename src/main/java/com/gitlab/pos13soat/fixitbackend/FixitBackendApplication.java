0830c7c9b3973cc2bf21748448ddff8b263a845d
Marcio Kleber Junior Lopes de Souza <marciokjls@gmail.com>
2025-10-18 13:09:23 -0300

    :tada: First commit - Criação do projeto Fix It (MVP - back-end de um sistema de oficina)

---
+++ src/main/java/com/gitlab/pos13soat/fixitbackend/FixitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gitlab.pos13soat.fixitbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixitBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FixitBackendApplication.class, args);
+    }
+
+}
