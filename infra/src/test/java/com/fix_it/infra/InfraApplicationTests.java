89688cc2501e3f39b8017aab4d89a1a7bea99952
Matheus Leal Gomes da Silva <matheuslealpa@gmail.com>
2026-01-08 17:47:44 -0300

    chore: comenta spring-test

--- infra/src/test/java/com/fix_it/infra/InfraApplicationTests.java
+++ infra/src/test/java/com/fix_it/infra/InfraApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class InfraApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
